a1046a2b8b920a982941106c233da9037bd3243c
edyrrg <edyrojas64@gmail.com>
2024-03-31 17:30:50 -0600

    dev interface user

--- src/controllers/input_handler.py
+++ src/controllers/input_handler.py
@@ -1,69 +1,71 @@
-def handler_input_option(lower_limit, upper_limit):
-    while True:
-        try:
-            user_response = int(input(">_: "))
-            if lower_limit <= user_response <= upper_limit:
-                return user_response
-            else:
-                print("Only numbers are accepted in the list of menu options")
-        except ValueError:
-            print("Enter only numbers")
+class InputHandler:
+    @staticmethod
+    def handler_input_option(lower_limit, upper_limit):
+        while True:
+            try:
+                user_response = int(input(">_: "))
+                if lower_limit <= user_response <= upper_limit:
+                    return user_response
+                else:
+                    print("Only numbers are accepted in the list of menu options")
+            except ValueError:
+                print("Enter only numbers")
 
+    @staticmethod
+    def handler_response_user(min_letters, text_err):
+        while True:
+            try:
+                user_response = input(">_: ")
+                if user_response is not None and len(user_response) >= min_letters:
+                    return user_response
+                else:
+                    print(f"Warning: {text_err}")
+            except ValueError as e:
+                print(e)
 
-def handler_response_user(min_letters):
-    while True:
-        try:
+    @staticmethod
+    def handler_number_response_user():
+        while True:
+            try:
+                user_response = int(input(">_: "))
+                if user_response > 0:
+                    return user_response
+                else:
+                    print("\t    Only numbers greater than zero")
+            except ValueError:
+                print("\t    Enter only numbers")
+
+    @staticmethod
+    def handler_price_value():
+        while True:
+            try:
+                user_response = float(input(">_: "))
+                if user_response > 0:
+                    return user_response
+                else:
+                    print("\t    Only numbers greater than zero")
+            except ValueError:
+                print("\t    Enter only numbers")
+
+    @staticmethod
+    def handler_yorn_response_user():
+        while True:
             user_response = input(">_: ")
-            if user_response is not None and len(user_response) >= min_letters:
-                return user_response
+            if user_response == "yes" or user_response == "y" or user_response == "Yes":
+                return True
+            elif user_response == "no" or user_response == "n" or user_response == "No":
+                return False
             else:
-                print(f"Enter valid name, valid data or valid description please")
-        except ValueError as e:
-            print(e)
+                print('Please enter: y(Yes) or n(No)')
 
-
-def handler_number_response_user():
-    while True:
-        try:
-            user_response = int(input(">_: "))
-            if user_response > 0:
-                return user_response
+    @staticmethod
+    def handler_continue_response_user():
+        while True:
+            user_response = input(">_: ")
+            if (user_response == "continue" or user_response == "c" or user_response == "yes"
+                    or user_response == "Yes" or user_response == "C" or user_response == "y"):
+                return True
+            elif user_response == "no" or user_response == "n" or user_response == "back":
+                return False
             else:
-                print("\t    Only numbers greater than zero")
-        except ValueError:
-            print("\t    Enter only numbers")
-
-
-def handler_price_value():
-    while True:
-        try:
-            user_response = float(input(">_: "))
-            if user_response > 0:
-                return user_response
-            else:
-                print("\t    Only numbers greater than zero")
-        except ValueError:
-            print("\t    Enter only numbers")
-
-
-def handler_yorn_response_user():
-    while True:
-        user_response = input(">_: ")
-        if user_response == "yes" or user_response == "y" or user_response == "Yes":
-            return True
-        elif user_response == "no" or user_response == "n" or user_response == "No":
-            return False
-        else:
-            print('Please enter: y(Yes) or n(No)')
-
-
-def handler_continue_response_user():
-    while True:
-        user_response = input(">_: ")
-        if (user_response == "continue" or user_response == "c" or user_response == "yes"
-                or user_response == "Yes" or user_response == "C" or user_response == "y"):
-            return True
-        elif user_response == "no" or user_response == "n" or user_response == "back":
-            return False
-        else:
-            print('Please enter: c(Continue) or n(No)')
+                print('Please enter: c(Continue) or n(No)')
--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,14 +1,75 @@
 from src.controllers.file_controller import FilesController
+from src.controllers.input_handler import InputHandler
 
 
 class GUI:
     def __init__(self):
-        self.path = self.get_input_user_path()
-        self.file_controller = FilesController()
-        self.init_main()
+        print("**** Welcome User ****")
+        self.my_name = "Edy Rolando Rojas González"
+        self.my_id = "201730511"
+        self.my_github_link = "https://github.com/edyrrg/IPC2_Proyecto2_201730511"
+        self.file_controller = self.get_user_path()
+        self.init_menu()
+        self.main_menu()
+        self.xml_service = None
 
-    def init_main(self):
+    def init_menu(self):
+        print("XML Files in dir", self.file_controller.directory)
+        while True:
+            self.file_controller.get_list_files()
+            upper_limit = self.file_controller.list_of_files.__len__() - 1
+            res_user = InputHandler.handler_input_option(0, upper_limit)
+            self.file_controller.set_file_by_index(res_user)
+            print("Is the following XML file correct?", self.file_controller.path_file)
+            print('Please enter: y(Yes) or n(No)')
+            res_correct = InputHandler.handler_yorn_response_user()
+            if res_correct:
+                return
+
+    def main_menu(self):
+        while True:
+            print("**** Main Menu ****")
+            print("1 ----- Init Program")
+            print("2 ----- Load an input XML file")
+            print("3 ----- Mockup management")
+            print("4 ----- Mockup resolution")
+            print("5 ----- Help!")
+            print("6 ----- Exit")
+            user_input = InputHandler.handler_input_option(1, 6)
+            if user_input == 1:
+                self.file_controller = self.get_user_path()
+            if user_input == 2:
+                self.init_menu()
+            if user_input == 3:
+                return
+            if user_input == 4:
+                return
+            if user_input == 5:
+                self.help_me()
+            if user_input == 6:
+                print("Ending Program...")
+                return
+    def mockup_manage(self):
         pass
 
-    def get_input_user_path(self):
-        pass
+    def help_me(self):
+        print("Hello User!")
+        print(f'My name is {self.my_name}')
+        print(f'My carne is {self.my_id}')
+        print(f'Documentation link: {self.my_github_link}')
+
+    @classmethod
+    def get_user_path(cls):
+        try:
+            while True:
+                print("Please enter path directory:")
+                res_user_path = InputHandler.handler_response_user(5,
+                                                                   "Please enter a valid directory path...")
+                tmp_files_controller = FilesController(res_user_path, ".xml")
+                if tmp_files_controller.is_correct_directory_path():
+                    print("Uploading .xml  files in directory...")
+                    print("Uploaded successfully!")
+                    return tmp_files_controller
+                print("Invalid directory path...")
+        except Exception as e:
+            print(e)
